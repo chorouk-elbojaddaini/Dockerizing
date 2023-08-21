afb3838304d96b71b105af5c7df466d8ce14d586
chorouk-elbojaddaini <choroukelbojaddaini001@gmail.com>
2023-08-21 21:36:13 +0100

    spring-security

---
+++ backend-security/src/main/java/com/backendsecurity/BackendSecurityApplication.java
@@ -0,0 +1,14 @@
+package com.backendsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSecurityApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
